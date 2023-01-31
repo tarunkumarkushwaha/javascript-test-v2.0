03a579f6ae13c761fe4e892f96e32a847c8de0e0
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-01-31 18:27:37 +0530

    timer 50% fixed

--- script.js
+++ script.js
@@ -1,59 +1,73 @@
 // document declerations 
 let frontpage = document.getElementById('frontpage')
-// let examsession = document.getElementById('examsession')
 let quespage = document.getElementById('quespage')
+let quesonpage = document.getElementById('question')
+let option1 = document.getElementById('option1')
+let option2 = document.getElementById('option2')
+let option3 = document.getElementById('option3')
+let option4 = document.getElementById('option4')
+let answer = document.getElementById('answer')
+let whos_next = document.getElementById('whos_next')
 let resultpage = document.getElementById('resultpage')
-let blankmarksheet = document.getElementById('blankmarksheet')
 let time = document.getElementById('time')
-let prevresult = document.getElementById('prevresponse')
+let nextbutton = document.querySelector('#whos_next');
 
-// // response outlet
-// let response = []
-// prevresult.innerText = localStorage.getItem("responses") ? localStorage.getItem("responses")
-//  : "Previous Records - no previous record, lets try for the best"
-// frontpage.append(prevresult)
 
-// question object testing
+// virtual get response for testing
 let questions = [
     {
-        question1: "1. What will be the output of the following code snippet var a = \"Scaler\" \n var result = a.substring(2, 4)\ndocument.write(result)",
+        question: "1. What will be the output of the following code snippet var a = \"Scaler\" \n var result = a.substring(2, 4)\ndocument.write(result)",
         option1: "calculate by your self, i can't do it",
         option2: "cal",
         option3: "al",
         option4: "ali",
-        correctresponse: "questions[0].option3" ,
-        time:  2
+        correctresponse: "questions[0].option3",
+        time: 2
     },
     {
-        question2: "1. What will be the output of the following code snippet var a = \"Scaler\" \n var result = a.substring(2, 4)\ndocument.write(result)",
-        option1: "calculate by your self, i can't do it",
-        option2: "cal",
-        option3: "al",
-        option4: "ali",
-        correctresponse: "questions[0].option3" ,
-        time:  2
+        question: "2. What will be the output of the following code snippet console.log(halwa)",
+        option1: "no halwa available !, please order on swingy",
+        option2: "halwa",
+        option3: "no output",
+        option4: "halwa is not defined",
+        correctresponse: "questions[0].option4",
+        time: 2
     }
 ]
 
 
 // buttons 
 const teststartbutton = () => {
-    countout()
     quespage.removeAttribute("class", "invisible")
     frontpage.removeAttribute("class", "frontpage")
     quespage.setAttribute("class", "visible")
     frontpage.setAttribute("class", "invisible")
     time.removeAttribute("class", "invisible")
-    console.log(questions[1])
+    time.innerHTML=countout(1)
+    whos_next_handler()
+}
+const getanswer = () => {
+    answer.innerHTML = questions[0].correctresponse
+    // eval(questions[i].correctresponse)
+}
+
+let i = 0
+const whos_next_handler = () => {
+    if (i < questions.length) {
+        // time.innerHTML=countout(questions[i].time)
+        quesonpage.innerHTML = questions[i].question;
+        option1.innerHTML = questions[i].option1;
+        option2.innerHTML = questions[i].option2;
+        option3.innerHTML = questions[i].option3;
+        option4.innerHTML = questions[i].option4;
+        console.log(questions[i].question);
+        i++
+        if (i == questions.length) { nextbutton.setAttribute("class", "invisible") }
+    }
 }
 const retry = () => {
     window.location.reload()
 }
-// const next question = () => {
-//     quespage1.setAttribute("class", "invisible")
-//     quespage2.removeAttribute("class", "invisible")
-//     quespage2.setAttribute("class", "visible")
-// }
 
 const resultpagehandler = () => {
     time.remove()
@@ -78,34 +92,16 @@ const startTimer = (duration, display) => {
 
         if (--timer < 0) {
             timer = duration;
-            resultpagehandler()
-            // console.log("your response is " , response)
+            whos_next_handler()
         }
     }, 1000);
 }
-const countout = () => {
-    let twoMin = 60 * 10;
-    // let twoMin = 10;
+const countout = (num) => {
+    let Min = 60 * num;
     let display = document.querySelector('#time');
-    startTimer(twoMin, display);
+    startTimer(Min, display);
 };
 
-// radios selections
-// ques 
-const btnsubmit = document.querySelector('#btnsubmit');
-const radioButtons = document.querySelectorAll('input[name="size"]');
-btnsubmit.addEventListener("click", () => {
-    let selectedSize;
-    for (const radioButton of radioButtons) {
-        if (radioButton.checked) {
-            selectedSize = radioButton.value;
-            break;
-        }
-    }
-    // save the response 
-    // selectedSize ? response.push(selectedSize) : response.push(false)
-    // blankmarksheet.innerHTML = `No of wrong ans - ${response.filter((ans) => ans != true).length}/5 and no of True ans - ${response.filter((ans) => ans != false).length}/5`
-    // localStorage.setItem("responses", `Previous result - ${blankmarksheet.innerText}`)
-    // console.log(response)
-});
+// event listeners
 btnsubmit.addEventListener("click", resultpagehandler);
+whos_next.addEventListener("click", whos_next_handler);
