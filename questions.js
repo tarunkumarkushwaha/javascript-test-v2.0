503a162d8d87bafc3b2c3044e867dbd80b95aab4
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-02-02 15:12:05 +0530

    Stable update1 - UI improvement and sounds added

---
+++ questions.js
@@ -0,0 +1,120 @@
+let questions = [
+    {
+        question: "1. What will be the output of the following code snippet var a = \"Scaler\" \n var result = a.substring(2, 4)\ndocument.write(result)",
+        option1: "calculate by your self, i can't do it",
+        option2: "cal",
+        option3: "al",
+        option4: "ali",
+        correctresponse: "questions[0].option3",
+        time: 1
+    },
+    {
+        question: "2. What will be the output of the following code snippet console.log(halwa)",
+        option1: "no halwa available !, please order on swiggy",
+        option2: "halwa",
+        option3: "no output",
+        option4: "halwa is not defined",
+        correctresponse: "questions[1].option4",
+        time: 1
+    },
+    {
+        question: "3. Pick the wrong",
+        option1: "Primitive data types can store only a single value.",
+        option2: "To store multiple and complex values, non-primitive data types are used",
+        option3: "Object is a primitive data type",
+        option4: "typeof of NaN will return a Number.",
+        correctresponse: "questions[2].option3",
+        time: 1
+    },
+    {
+        question: "4. Pick the correct one",
+        option1: " == is used to compare values while === is used to compare values and data type both",
+        option2: "To store multiple and complex values, non-primitive data types are used",
+        option3: "Object is a primitive data type",
+        option4: "JS is java of internet",
+        correctresponse: "questions[3].option1",
+        time: 1
+    },
+    {
+        question: "5. JavaScript is a ______ language.",
+        option1: "dynamically typed",
+        option2: "statically typed language",
+        option3: "very very very 100X tough",
+        option4: "internet",
+        correctresponse: "questions[4].option1",
+        time: 1
+    },
+    {
+        question: "6. What is IIFE in JavaScript.",
+        option1: "Award show to feature best JS devoloper",
+        option2: " Immediately Invoked Function(IIFE) is a function that runs as soon as it is defined.",
+        option3: "Immediately Invoked Function(IIFE) is a function to listen events",
+        option4: "syntaxError- Immediately Invoked Function(IIFE) is not a function",
+        correctresponse: "questions[5].option2",
+        time: 1
+    },
+    {
+        question: "7. What do you mean by strict mode in javascript and characteristics of javascript strict-mode",
+        option1: "nothing special it is just a gimmic",
+        option2: "It is a new feature by which all forms of errors, including silent errors, will be thrown. As a result, debugging becomes a lot simpler.",
+        option3: "It is a feature which is only used in reactjs in rendering components",
+        option4: "It is a new feature by which all forms of errors, including silent errors will be removed and the script is autocorrected",
+        correctresponse: "questions[6].option2",
+        time: 1
+    },
+    {
+        question: "8. What signifies the difference between Java and JavaScript?",
+        option1: "JavaScript code can run <b>only</b> in the browser and Java can be run everywhere",
+        option2: "javascript is a part of java to make web apps",
+        option3: "Let me google it, i will find it on stackoverflow",
+        option4: "Java creates applications that can run in any virtual machine (JVM) or browser while JavaScript was made as a programming language to make web pages interactive",
+        correctresponse: "questions[7].option4",
+        time: 1
+    },
+    {
+        question: "9. Pick the wrong one",
+        option1: "When a number is added to a string, the number type is always converted to the number type.",
+        option2: "false, 0, 0n, -0, “”, null, undefined, and NaN are falsey values",
+        option3: "OR (||) operator - If the first value is truthy, then the first value is returned. Otherwise, always the second value gets returned.",
+        option4: "AND ( && ) operator - If both the values are truthy, always the second value is returned. If the first value is falsy then the first value is returned or if the second value is falsy then the second value is returned.",
+        correctresponse: "questions[8].option1",
+        time: 2
+    },
+    {
+        question: "10. Which can used to access HTML elements using Javascript??",
+        option1: "document.queryselector",
+        option2: "document.getElementbyID",
+        option3: "document.getElementById",
+        option4: "document.querySelection",
+        correctresponse: "questions[9].option3",
+        time: 1
+    },
+    {
+        question: "11. typeof of NaN will return a Number.",
+        option1: "To check if a value is NaN, we use the isNaN() function",
+        option2: "NaN means not a number so NaN will not return a number",
+        option3: "typeof NaN will not work on NaN",
+        option4: "the statement in question and other options are wrong",
+        correctresponse: "questions[10].option1",
+        time: 1
+    },
+    {
+            question: "12. Functions that are used as an argument to another function are called callback functions.",
+            option1: "Callback functions are always cause callback hell",
+            option2: "Functions that are used as an argument to another function are called fNfunctions.",
+            option3: "Callback functions are constructor functions, which are used to create objects in javascript.",
+            option4: "the statement in question is correct and option1, option2 and option3 are wrong",
+            correctresponse: "questions[11].option4",
+            time: 1
+        }
+    // ,{
+        //     question: "13. ?",
+        //     option1: "",
+        //     option2: "",
+        //     option3: "",
+        //     option4: "",
+        //     correctresponse: "questions[12].option2",
+        //     time: 1
+        // }
+]
+export {questions}
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { questions } from "/questions.js"
 // document declerations 
 let frontpage = document.getElementById('frontpage')
 let quespage = document.getElementById('quespage')
@@ -6,49 +7,67 @@ let option1 = document.getElementById('option1')
 let option2 = document.getElementById('option2')
 let option3 = document.getElementById('option3')
 let option4 = document.getElementById('option4')
+let passPercentage = document.getElementById('passPercentage')
 let answer = document.querySelector('#answer')
+let Remarks = document.querySelector('#Remarks')
 let prevresponse = document.querySelector('#prevresponse')
 let blankmarksheet = document.querySelector('#blankmarksheet')
 let whos_next = document.getElementById('whos_next')
 let resultpage = document.getElementById('resultpage')
 let time = document.getElementById('time')
 let nextbutton = document.querySelector('#whos_next');
+let startbutton = document.querySelector('#teststartbutton');
+let retrybutton = document.querySelector('#Retry');
 let optionselection = document.querySelectorAll('#option1, #option2, #option3, #option4');
 
 // audios 
 let trueSound = new Audio("https://cdn.pixabay.com/download/audio/2022/03/15/audio_b8c9103636.mp3?filename=correct-83487.mp3");
 let falseSound = new Audio("https://cdn.pixabay.com/download/audio/2021/08/04/audio_c6ccf3232f.mp3?filename=negative_beeps-6008.mp3");
-// virtual get response for testing
-let questions = [
+
+// mock questions for testing
+// let questions = [
+//     {
+//         question: "1. eval(5+2)",
+//         option1: "calculate by your self, i can't do it",
+//         option2: "52",
+//         option3: "7",
+//         option4: "error",
+//         correctresponse: "questions[0].option3",
+//         time: 2
+//     }
+// ]
+
+// Remarks 
+let quotes = [
     {
-        question: "1. What will be the output of the following code snippet var a = \"Scaler\" \n var result = a.substring(2, 4)\ndocument.write(result)",
-        option1: "calculate by your self, i can't do it",
-        option2: "cal",
-        option3: "al",
-        option4: "ali",
-        correctresponse: "questions[0].option3",
-        time: 2
+        zero: "Once you learn to quit, it becomes a habit, So don't quit",
+        sound: "https://cdn.pixabay.com/download/audio/2022/06/08/audio_e5d4fb5405.mp3?filename=no-luck-too-bad-disappointing-sound-effect-112943.mp3"
     },
     {
-        question: "2. What will be the output of the following code snippet console.log(halwa)",
-        option1: "no halwa available !, please order on swiggy",
-        option2: "halwa",
-        option3: "no output",
-        option4: "halwa is not defined",
-        correctresponse: "questions[1].option4",
-        time: 1
+        per1020: "To aim is not enough, you must hit!",
+        sound: "https://cdn.pixabay.com/download/audio/2022/03/10/audio_00a12a8dc1.mp3?filename=piano-crash-sound-37898.mp3"
     },
     {
-        question: "3. What will be the dinner",
-        option1: "Paneer butter masala ",
-        option2: "Only one and only Dudh roti",
-        option3: "Veg + dudh roti",
-        option4: "Aloo paratha",
-        correctresponse: "questions[2].option2",
-        time: 1
+        per3050: "Our greatest glory is not in never failing, but in getting up every time we do. ~ Confucius",
+        sound: "https://cdn.pixabay.com/download/audio/2021/08/04/audio_bf3620f48d.mp3?filename=yay-6120.mp3"
+    },
+    {
+        per60: "To think is easy. To act is hard. But the hardest thing in the world is to act in accordance with your thinking.",
+        sound: "https://cdn.pixabay.com/download/audio/2022/01/18/audio_2722574464.mp3?filename=badge-coin-win-14675.mp3"
+    },
+    {
+        per7080: "The important thing is to dare to dream big, then take action to make it come true.",
+        sound: "https://cdn.pixabay.com/download/audio/2021/08/09/audio_3047f54f23.mp3?filename=winfantasia-6912.mp3"
+    },
+    {
+        per90: "Perfection is not attainable, but if we chase perfection we can catch excellence.",
+        sound: "https://cdn.pixabay.com/download/audio/2021/08/04/audio_0625c1539c.mp3?filename=success-1-6297.mp3"
+    },
+    {
+        per100: "Pleasure in the job puts perfection in the work. ~ Aristotle",
+        sound: "https://cdn.pixabay.com/download/audio/2021/08/04/audio_12b0c7443c.mp3?filename=success-fanfare-trumpets-6185.mp3"
     }
 ]
-
 // Preset 
 prevresponse.innerHTML = `Previous record- Correct responses - ${JSON.parse(localStorage.getItem('items')) ? JSON.parse(localStorage.getItem('items')).length : 0} 
 and Incorrect responses - ${JSON.parse(localStorage.getItem('items')) ? questions.length - JSON.parse(localStorage.getItem('items')).length : 0}`
@@ -56,24 +75,25 @@ let answerarray = []
 answer.setAttribute("class", ("answerInvisible"))
 
 // buttons 
-const teststartbutton = () => {
+const teststart = () => {
     localStorage.clear()
     quespage.removeAttribute("class", "invisible")
     frontpage.removeAttribute("class", "frontpage")
     quespage.setAttribute("class", "visible")
     frontpage.setAttribute("class", "invisible")
     time.removeAttribute("class", "invisible")
-    time.innerHTML = countout(1)
+    time.innerHTML = countout(10)
     whos_next_handler()
 }
 
 let i = 0
 let j = ""
+let percentage
 const whos_next_handler = () => {
     answer.hasAttribute("answerVisible") ? console.log("gadbad") : answer.setAttribute("class", "answerInvisible")
     optionselection.forEach(optionselection => optionselection.addEventListener('click', getAnswer));
     if (i < questions.length) {
-        time.innerHTML = "01:00"
+        time.innerHTML = "..wait"
         quesonpage.innerHTML = questions[i].question;
         option1.innerHTML = questions[i].option1;
         option2.innerHTML = questions[i].option2;
@@ -100,6 +120,37 @@ const resultpagehandler = () => {
     localStorage.setItem("items", JSON.stringify(answerarray))
     time.remove()
     quespage.remove()
+    percentage = (answerarray.length/questions.length) * 100
+    passPercentage.innerHTML = percentage + "%"
+    if (percentage == 0) {
+        new Audio(quotes[0].sound).play()
+        Remarks.innerHTML = quotes[0].zero
+    }
+    else if (percentage > 0 && percentage <= 30) {
+        new Audio(quotes[1].sound).play()
+        Remarks.innerHTML = quotes[1].per1020
+    }
+    else if (percentage > 30 && percentage <= 59) {
+        new Audio(quotes[2].sound).play()
+        Remarks.innerHTML = quotes[2].per3050
+    }
+    else if (percentage > 59 && percentage <= 69) {
+        new Audio(quotes[3].sound).play()
+        Remarks.innerHTML = quotes[3].per60
+    }
+    else if (percentage > 69 && percentage <= 89) {
+        new Audio(quotes[4].sound).play()
+        Remarks.innerHTML = quotes[4].per7080
+    }
+    else if (percentage > 90 && percentage <= 99) {
+        new Audio(quotes[5].sound).play()
+        Remarks.innerHTML = quotes[2].per90
+    }
+    else {
+        new Audio(quotes[6].sound).play()
+        Remarks.innerHTML = quotes[6].per100
+    }
+    move()
     resultpage.removeAttribute("class", "invisible")
     resultpage.setAttribute("class", "visible")
 }
@@ -133,3 +184,25 @@ const countout = (num) => {
 // event listeners
 btnsubmit.addEventListener("click", resultpagehandler);
 whos_next.addEventListener("click", whos_next_handler);
+startbutton.addEventListener("click", teststart);
+retrybutton.addEventListener("click", retry);
+
+// progressbar
+var k = 0;
+function move() {
+    if (k == 0) {
+        k = 1;
+        var elem = document.getElementById("myBar");
+        var width = 1;
+        var id = setInterval(frame, 20);
+        function frame() {
+            if (width >= percentage) {
+                clearInterval(id);
+                k = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
